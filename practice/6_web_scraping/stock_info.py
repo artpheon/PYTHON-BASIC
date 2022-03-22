0b00f4bb50df860f58aa9ef440680722e044359f
Artur Nigmatullin <anigmatullin@griddynamics.com>
2022-03-22 18:36:13 +0400

    6_web_scraping done (without tests)

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -31,4 +31,270 @@ Links:
     - beautiful soup docs: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
     - lxml docs: https://lxml.de/
 """
+import random
+from bs4 import BeautifulSoup as BS
+import requests
+import re
+import logging
+from prettytable import PrettyTable
+import json
 
+
+class StockInfo:
+    DEBUG = False
+    BASE_URL = 'https://finance.yahoo.com'
+    MOST_ACTIVE = BASE_URL + '/most-active'
+    COMPANIES_NUM = 0
+    BLK = 'BLK'
+
+    def _prepare_session(self):
+        with open('user_agents.json', 'r') as fp:
+            self.headers_list = json.load(fp)
+        self._update_session()
+
+    def _update_session(self):
+        self.session = requests.Session()
+        self.session.headers = random.choice(self.headers_list)
+        logging.info('updating session and user-agent')
+
+    def _set_logging(self):
+        if self.DEBUG:
+            fmt = '%(asctime)s — %(name)s — %(funcName)s:%(lineno)d — %(message)s'
+            logging.basicConfig(filename='stock_info.log', filemode='w',
+                                format=fmt,
+                                level=logging.DEBUG)
+
+    def __init__(self):
+        self.companies = dict()
+        self._prepare_session()
+        self._set_logging()
+
+    def get_soup_from_url(self, url, params=None):
+        page = self.session.get(url, params=params)
+        if not page.ok:
+            raise requests.exceptions.ConnectionError
+        return BS(page.content, 'html.parser').body
+
+    @staticmethod
+    def re_parse_results(string):
+        comp = re.compile(r'.*?of (\d+) results.*?')
+        return int(comp.search(string).group(1))
+
+    def gen_listing_soups(self):
+        count = 100
+        n = self.COMPANIES_NUM // count + 1
+        for i in range(n):
+            logging.info(f'getting page at count: {count}, offset: {i * count}')
+            yield self.get_soup_from_url(
+                self.MOST_ACTIVE,
+                {'count': count, 'offset': i * count}
+            )
+
+    @staticmethod
+    def parse_info_from_trows(soup):
+        table = soup.select('div#scr-res-table>div>table>tbody>tr')
+        logging.info(f'Found {len(table)} rows in a table')
+        result = dict()
+        for tr in table:
+            link = tr.select_one('a')
+            result[link.text] = dict(title=link['title'])
+        return result
+
+    def get_basic_info(self):
+        soup = self.get_soup_from_url(self.MOST_ACTIVE)
+        res_num = soup.find(
+            'span',
+            {'class': ""},
+            text=lambda text: text and 'results' in text).text
+        self.COMPANIES_NUM = self.re_parse_results(res_num)
+        logging.info(f'Found {self.COMPANIES_NUM} results in total.')
+        links_generator = self.gen_listing_soups()
+        for link in links_generator:
+            self.companies.update(self.parse_info_from_trows(link))
+
+    def collect_statistics(self, code):
+        """Collects 52-Week Change, Total Cash"""
+        soup = self.get_soup_from_url(f'{self.BASE_URL}/quote/{code}/key-statistics')
+        section = soup.select_one('div#Main').find('section')
+        if not section:
+            raise requests.exceptions.ConnectionError
+        tcells = section.find_all('td')
+        week_change = total_cash = None
+        for cell in tcells:
+            if cell.text == '52-Week Change 3':
+                week_change = cell.next_sibling.text.strip('%')
+            if cell.text == 'Total Cash (mrq)':
+                total_cash = cell.next_sibling.text
+        if week_change and week_change != 'N/A':
+            week_change = float(week_change)
+        else:
+            week_change = None
+        self.companies[code]['statistics'] = {
+            'week_change': week_change,
+            'total_cash': total_cash,
+        }
+
+    def collect_profile(self, code):
+        """ Collects Country, Employees, CEO Name, CEO Year Born"""
+        try:
+            soup = self.get_soup_from_url(f'{self.BASE_URL}/quote/{code}/profile')
+            section = soup.select_one('div#Main').find('section')
+            country = section.p.a.previous_sibling.previous_sibling.text
+            employees = section.find_all('p')[1].find_all('span')[-1].text
+            tr = section.tbody.tr.find_all('span')
+            ceo_name = tr[0].text
+            ceo_year = tr[-1].text
+            ceo_year = int(ceo_year) if (ceo_year and ceo_year != 'N/A') else None
+        except Exception as exc:
+            raise requests.exceptions.ConnectionError from exc
+        else:
+            self.companies[code]['profile'] = {
+                'country': country,
+                'employees': employees,
+                'ceo_name': ceo_name,
+                'ceo_year': ceo_year,
+            }
+
+    def collect_holders(self, code):
+        """Collects Shares, Date Reported, % Out, Value."""
+        try:
+            soup = self.get_soup_from_url(f'{self.BASE_URL}/quote/{code}/holders')
+            section = soup.select_one('div#Main').section
+            trows = section.find(
+                text='Top Institutional Holders'
+            ).next_element.find('tbody').find_all('tr')
+        except Exception as exc:
+            raise requests.exceptions.ConnectionError from exc
+        else:
+            if code not in self.companies:
+                self.companies[code] = dict()
+            holders = dict()
+            i = 1
+            for row in trows:
+                holders[i] = dict()
+                vals = [str(s) for s in list(row.strings)]
+                holders[i]['name'] = vals[0]
+                holders[i]['shares'] = vals[1]
+                holders[i]['date_reported'] = vals[2]
+                holders[i]['percent_out'] = vals[3]
+                holders[i]['value'] = vals[4]
+                i = i + 1
+            self.companies[code]['holders'] = holders
+
+    def collect_blk_holders(self):
+        try:
+            self.collect_holders(self.BLK)
+        except requests.exceptions.ConnectionError:
+            self._update_session()
+            try:
+                self.collect_holders(self.BLK)
+            except requests.exceptions.ConnectionError:
+                logging.warning('Black Rock holdings data have not been collected')
+
+    def _get_full_info(self, code):
+        try:
+            self.collect_statistics(code)
+            self.collect_profile(code)
+        except requests.exceptions.ConnectionError:
+            self._update_session()
+            try:
+                self.collect_statistics(code)
+                self.collect_profile(code)
+            except requests.exceptions.ConnectionError:
+                logging.warning(f'Some data for {code} have not been collected.')
+
+    def get_full_info(self):
+        logging.info(f'Scraped {len(self.companies)} results')
+        i = 1
+        for k in self.companies:
+            print(f'Collecting full data for {k}: ', end='')
+            self._get_full_info(k)
+            print('(OK)')
+            if i % 10 == 0:
+                print(f'Done: {i / self.COMPANIES_NUM * 100:.1f}%.')
+            i += 1
+
+    def close(self):
+        self.session.close()
+
+    def save_json(self, name):
+        with open(name, 'w') as fp:
+            json.dump(obj=self.companies, fp=fp, indent=4, sort_keys=True)
+
+    def find_five_youngest(self):
+        rows = []
+        for k, v in self.companies.items():
+            if 'profile' in v and v['profile']['ceo_year']:
+                rows.append((k, v['profile']['ceo_year']))
+        return sorted(rows, reverse=True, key=lambda x: x[1])[:5]
+
+    def sheet_youngest_ceo(self):
+        """5 stocks with the youngest CEOs"""
+        x = PrettyTable()
+        x.title = '5 stocks with the youngest CEOs'
+        x.field_names = ['Name', 'Code', 'Country', 'Employees', 'CEO Name', 'CEO Year']
+        youngest = self.find_five_youngest()
+        for row in youngest:
+            code = row[0]
+            year = row[1]
+            title = self.companies[code]['title']
+            country = self.companies[code]['profile']['country']
+            employees = self.companies[code]['profile']['employees'] or 'N/A'
+            ceo = self.companies[code]['profile']['ceo_name']
+            x.add_row([title, code, country, employees, ceo, year])
+        x.align = 'l'
+        return x
+
+    def find_best_52_week_change(self):
+        best_change = []
+        for k, v in self.companies.items():
+            if 'statistics' in v and v['statistics']['week_change']:
+                best_change.append((k, v['statistics']['week_change']))
+        return sorted(best_change, reverse=True, key=lambda x: x[1])[:10]
+
+    def sheet_best_52_change(self):
+        """10 stocks with the best 52-week change"""
+        x = PrettyTable()
+        x.title = '10 stocks with the best 52-week change'
+        x.field_names = ['Name', 'Code', '52-Week Change', 'Total Cash']
+        best_change = self.find_best_52_week_change()
+        for row in best_change:
+            code = row[0]
+            change = f'{row[1]}%'
+            title = self.companies[code]['title']
+            total_cash = self.companies[code]['statistics']['total_cash']
+            x.add_row([title, code, change, total_cash])
+        x.align = 'l'
+        return x
+
+    def sheet_10_holds_blk(self):
+        """10 largest holds of Blackrock Inc"""
+        x = PrettyTable()
+        x.title = '10 largest holds of Blackrock Inc'
+        x.field_names = ['Name', 'Shares', 'Date Reported', '% Out', 'Value']
+        holders = self.companies['BLK']['holders']
+        for v in holders.values():
+            x.add_row(
+                [v['name'], v['shares'], v['date_reported'], v['percent_out'], v['value']]
+            )
+        x.align = 'l'
+        return x
+
+
+if __name__ == '__main__':
+    info = StockInfo()
+    print('Collecting basic info about companies... ', end='')
+    info.get_basic_info()
+    print(f'(OK) Collected {info.COMPANIES_NUM} companies in total.')
+    info.get_full_info()
+    info.collect_blk_holders()
+    print('Done: 100%')
+    info.save_json('data.json')
+    info.close()
+
+    with open('table_youngest_ceo.txt', 'w') as f:
+        f.write(info.sheet_youngest_ceo().get_string() + '\n')
+    with open('table_blackrock_top_holders.txt', 'w') as f:
+        f.write(info.sheet_10_holds_blk().get_string() + '\n')
+    with open('table_best_52_week_change.txt', 'w') as f:
+        f.write(info.sheet_best_52_change().get_string() + '\n')
